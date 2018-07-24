86a39e9d90cb7b3593bdcc3849319b1413d63c26
ali-v-1985 <ali.valizadeh.h@gmail.com>
2018-07-25 01:27:19 +0430

    Modify bubble sort algorithm

--- src/main/java/me/valizadeh/practice/algorithm/sort/BubbleSort.java
+++ src/main/java/me/valizadeh/practice/algorithm/sort/BubbleSort.java
@@ -1,8 +1,5 @@
 package me.valizadeh.practice.algorithm.sort;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Comparator;
 
 /**
@@ -10,32 +7,18 @@ import java.util.Comparator;
  */
 public class BubbleSort<T> implements Sort<T> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(BubbleSort.class);
-
-
     @Override
     public T[] sort(T[] arr, Comparator<T> comparator) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (T t1 : arr) {
-            stringBuilder.append(t1).append(",");
-        }
-        stringBuilder.append("\r\n");
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = arr.length - 1; j > i; j--) {
-                if (comparator.compare(arr[j], arr[j - 1]) < 0) {
+        for (int i = arr.length; i >= 0; i--) {
+            for (int j = 0; j < i - 1; j++) {
+                if (comparator.compare(arr[j], arr[j + 1]) > 0) {
                     T swap = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = swap;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = swap;
                 }
-                for (T t1 : arr) {
-                    stringBuilder.append(t1).append(",");
-                }
-                stringBuilder.append("\r\n");
             }
 
         }
-
-        LOGGER.info(stringBuilder.toString());
         return arr;
     }
 }
